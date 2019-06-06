14db4737456501eae222220c0ffde1d17fd6ea9b
Apple <apple@Apples-MacBook-Pro-106.local>
2019-06-06 16:28:24 -0500

    First commit

---
+++ passwordCodingChallenge/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  passwordCodingChallenge
+//
+//  Created by Apple on 6/6/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var label2: UILabel!
+    
+    @IBAction func textBox(_ sender: UITextField) {
+    }
+    
+}
+
